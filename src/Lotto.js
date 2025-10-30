ed8514e3d3a6111958fdb5d6763957b25ea0f565
hjkim0905 <eax9952@gmail.com>
2025-10-30 18:07:24 +0900

    feat: add Lotto class validation logic using TDD and setup MVC architecture

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,22 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    const uniqueArr = new Set(numbers);
+    if (numbers.length !== uniqueArr.size) {
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
+    }
+
+    if (!numbers.every((number) => Number.isInteger(number))) {
+      throw new Error('[ERROR] 로또 번호에 정수가 아닌 값이 있습니다.');
+    }
+
+    if (!numbers.every((number) => number < 46 && number > 1)) {
+      throw new Error('[ERROR] 로또 번호에 1부터 45 사이가 아닌 값이 있습니다.');
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
